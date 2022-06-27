19256ba7486e49bfd37c2f377986a217b0e096b7
Adinda Nisa Anggraini <aadindanisa@gmail.com>
2022-06-27 23:35:52 +0700

    fix: add login function

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const logger = require('morgan')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
